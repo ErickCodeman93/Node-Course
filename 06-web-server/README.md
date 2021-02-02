b00014c5600c3271094914af0f520c21d65b07d7
Erick93Whiskey <erick@whiskeyindustries.net>
2021-02-01 18:43:20 -0600

    feat:Servidor Express, hbs, heroku básico

---
+++ 06-web-server/README.md
@@ -0,0 +1,3 @@
+### Servidor Express, hbs, heroku básico
+
+Realizamos un webServer con Express y creamos una estructura básica para contenido estático con handlebars.js y el deploy fue realizdo en la plataforma de heroku. 
--- 06-web-server/server.js
+++ 06-web-server/server.js
@@ -1,7 +1,39 @@
 const express = require('express');
 const app = express();
- 
+
+const hbs = require('hbs');
+require( './hbs/helpers' );
+
+const port = process.env.PORT || 3000;
+
 app.use( express.static(  __dirname + '/public' ) );
+
+//Express HBS engine
+hbs.registerPartials( __dirname + '/views/parciales' );
+app.set('view engine', 'hbs');
+
+// Urls 
+app.get('/', (req, res) => {
+
+	res.render( 'home',{
+		nombre : 'ErIcK AlvA'
+	} );
+});
+
+app.get('/about', (req, res) => {
+
+	res.render( 'about',{
+		img: 'https://loremflickr.com/320/240'
+	} );
+});
+ 
+app.get('/data', (req, res) => {	
+	res.send('Hola Mundo');
+});
+
+app.listen( port , () => {
+	console.log( `Escuchando en el puerto ${ port }` );
+});
 
 // app.get('/', (req, res) => {
 
@@ -13,11 +45,3 @@ app.use( express.static(  __dirname + '/public' ) );
 
 // 	res.send( salida );
 // })
- 
-app.get('/data', (req, res) => {	
-	res.send('Hola Mundo');
-})
-
-app.listen(3000, () => {
-	console.log( 'Escuchando en el puerto 3000' );
-});
