edbaa12e841e1be3497f70591606b3f519dc7760
Erick93Whiskey <erick@whiskeyindustries.net>
2021-02-08 18:57:19 -0600

    feat:CRUD, COLECCIONES

---
+++ 11-colecciones-CRUD/server/config/config.js
@@ -0,0 +1,36 @@
+// ======================================
+// Puerto
+// ======================================
+process.env.PORT = process.env.PORT || 3000;
+
+// ======================================
+// Entorno
+// ======================================
+process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
+
+// ======================================
+// Base de datos
+// ======================================
+let urlDB;
+
+// if( process.env.NODE_ENV === 'dev' )
+// 	urlDB = 'mongodb://localhost:27017/cafe';
+// else
+urlDB = process.env.MONGO_URI;
+
+process.env.URLDB = urlDB;
+
+// ======================================
+// Vencimiento del token
+// ======================================
+process.env.CADUCIDAD_TOKEN = '48h';
+
+// ======================================
+// SEED de autenticación
+// ======================================
+process.env.SEED = process.env.SEED || 'secretDev';
+
+// ======================================
+// Google Client
+// ======================================
+process.env.CLIENT_ID = process.env.CLIENT_ID || 'token';
---
+++ 11-colecciones-CRUD/server/server.js
@@ -0,0 +1,36 @@
+require( './config/config' );
+
+const express = require('express');
+const mongoose = require('mongoose');
+const path = require( 'path' ); 
+
+const app = express();
+
+const bodyParser = require('body-parser');
+
+// parse application/x-www-form-urlencoded
+app.use( bodyParser.urlencoded( { extended: false } ) );
+ 
+// parse application/json
+app.use( bodyParser.json() );
+ 
+app.use( express.static( path.resolve( __dirname, '../public' ) ) )
+
+//Configuracion global de rutas
+app.use( require( './routes/api' ) );
+
+mongoose.connect( process.env.URLDB, {
+	useNewUrlParser: true,
+	useUnifiedTopology: true,
+	useFindAndModify: false,
+	useCreateIndex: true
+}, (err, resp) => {
+    if (err) throw err;
+    console.log('Base de datos ONLINE');
+});
+
+app.listen( process.env.PORT, () => {
+	console.log( `Escuchando en el puerto ${ process.env.PORT }` );
+})
+
+
